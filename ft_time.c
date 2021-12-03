9acfe55b8454ca1e881436a7a052a1f182437a25
Tixtex <tixtex666@gmail.com>
2021-12-03 16:08:51 +0100

    update tempo

--- ft_time.c
+++ ft_time.c
@@ -11,6 +11,20 @@ double	ft_time_diff(struct timeval start, struct timeval end)
 		+ 1e-6 * (end.tv_usec - start.tv_usec));
 }
 
+struct timeval ft_time_add(struct timeval source, struct timeval add)
+{
+	struct timeval	aux;
+
+	aux.tv_sec = source.tv_sec + add.tv_sec;
+	aux.tv_sec = source.tv_sec + add.tv_sec;
+	if (aux.tv_usec >= 1000000)
+	{
+		aux.tv_sec += 1;
+		aux.tv_usec -= 1000000;
+	}
+	return (aux);
+}
+
 struct timeval	ft_wait_time(struct timeval time, double time_wait)
 {
 	struct timeval	time_init;
@@ -20,6 +34,9 @@ struct timeval	ft_wait_time(struct timeval time, double time_wait)
 	time_now = time;
 	while (time_wait > ft_time_diff(time_init, time_now))
 		gettimeofday(&time_now, NULL);
-	time_now = time_init + time_wait;
+	time_now.tv_sec = 0;
+	time_now.tv_usec = time_wait;
+	time_now = ft_time_add(time_init, time_now);//
+
 	return (time_now);
 }
